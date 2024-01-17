89e27f116aafe85d81c9f034011ca1bfee56caf8
Bebsti <129532033+Bebsti@users.noreply.github.com>
2024-01-17 18:54:41 +0530

    Update tiktok.py

--- plugins/tiktok.py
+++ plugins/tiktok.py
@@ -7,7 +7,7 @@ from bot import LOG_GROUP,DUMP_GROUP
 async def link_handler(Mbot, message):
     link = message.matches[0].group(0)
     try:
-        m = await message.reply_sticker("CAACAgIAAxkBATWhF2Qz1Y-FKIKqlw88oYgN8N82FtC8AAJnAAPb234AAT3fFO9hR5GfHgQ")
+        m = await message.reply_sticker("CAACAgUAAxkBAAIKI2Wn01-4FLtsSmgsP1OoZDz9NdGlAALpAQACvSDSJtCwRY2pJYUcNAQ")
         get_api= requests.post("https://lovetik.com/api/ajax/search",data={"query":link}).json()
         if get_api['status'] and "Invalid TikTok video url" in get_api['mess']: 
            return await message.reply("Oops Invalid TikTok video url. Please try again :) ")
@@ -18,14 +18,14 @@ async def link_handler(Mbot, message):
                      await message.reply_photo(get_api['cover'])
                  except:
                      pass 
-              dump_file = await message.reply_video(get_api['links'][0]['a'], caption="Thank you for using - @InstaReelsdownbot")
+              dump_file = await message.reply_video(get_api['links'][0]['a'], caption="Thank you for using -@Instagram_downloaderxbot")
            except KeyError:
                return await message.reply("Invalid TikTok video url. Please try again.")
            except Exception:
                snd_msg=await message.reply(get_api['links'][0]['a'])
                await asyncio.sleep(1)
                try:
-                  dump_file = await message.reply_video(get_api['links'][0]['a'],caption="Thank you for using - @InstaReelsdownbot")
+                  dump_file = await message.reply_video(get_api['links'][0]['a'],caption="Thank you for using - @Instagram_downloaderxbot")
                   await snd_msg.delete()
                except Exception:
                    pass
@@ -38,4 +38,4 @@ async def link_handler(Mbot, message):
             if DUMP_GROUP:
                await dump_file.copy(DUMP_GROUP)
             await m.delete()
-        await message.reply("Check out @spotify_downloa_bot(music)  @spotifynewss(Channel) \n Please Support Us By /donate To Maintain This Project")
+        await message.reply("Please Support Us By /donate To Maintain This Project")
